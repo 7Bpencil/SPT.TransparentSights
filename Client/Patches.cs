2a3a1f0e9bab2ab6a840f14d362cd8297ebfc989
7Bpencil <Edward.Ekb@yandex.ru>
2026-06-23 16:50:37 +0500

    codestyle

--- Client/Patches.cs
+++ Client/Patches.cs
@@ -26,98 +26,68 @@ using FirearmController = EFT.Player.FirearmController;
 
 namespace SevenBoldPencil.TransparentSights
 {
-    public struct ProceduralWeaponAnimation_Proxy
+    public struct ProceduralWeaponAnimation_Proxy(ProceduralWeaponAnimation instance)
     {
+        private readonly ProceduralWeaponAnimation __instance = instance;
+
         private static TypedFieldInfo<ProceduralWeaponAnimation, FirearmController> __firearmController = new("_firearmController");
         private static TypedFieldInfo<ProceduralWeaponAnimation, GInterface200> __firearmAnimationData = new("_firearmAnimationData");
         private static TypedFieldInfo<ProceduralWeaponAnimation, bool> __isAiming = new("_isAiming");
         private static TypedFieldInfo<ProceduralWeaponAnimation, int> __pose = new("_pose");
 
-        private ProceduralWeaponAnimation __instance;
-
         public FirearmController _firearmController { get { return __firearmController.Get(__instance); } set { __firearmController.Set(__instance, value); } }
         public GInterface200 _firearmAnimationData { get { return __firearmAnimationData.Get(__instance); } set { __firearmAnimationData.Set(__instance, value); } }
         public bool _isAiming { get { return __isAiming.Get(__instance); } set { __isAiming.Set(__instance, value); } }
         public int _pose { get { return __pose.Get(__instance); } set { __pose.Set(__instance, value); } }
-
-        public ProceduralWeaponAnimation_Proxy(ProceduralWeaponAnimation instance)
-        {
-            __instance = instance;
-        }
     }
 
-    public struct FirearmController_Proxy
+    public struct FirearmController_Proxy(FirearmController instance)
     {
+        private readonly FirearmController __instance = instance;
+
         private static TypedFieldInfo<FirearmController, Player> __player = new("_player");
 
-        private FirearmController __instance;
-
         public Player _player { get { return __player.Get(__instance); } set { __player.Set(__instance, value); } }
-
-        public FirearmController_Proxy(FirearmController instance)
-        {
-            __instance = instance;
-        }
     }
 
-	public struct WeaponPreview_Proxy
+	public struct WeaponPreview_Proxy(WeaponPreview instance)
 	{
+        private readonly WeaponPreview __instance = instance;
+
 		private static TypedFieldInfo<WeaponPreview, GameObject> __gameObject_0 = new("gameObject_0");
 		private static TypedFieldInfo<WeaponPreview, Item> __item_0 = new("item_0");
 
 		public GameObject gameObject_0 { get { return __gameObject_0.Get(__instance); } set { __gameObject_0.Set(__instance, value); } }
 		public Item item_0 { get { return __item_0.Get(__instance); } set { __item_0.Set(__instance, value); } }
-
-        private WeaponPreview __instance;
-
-        public WeaponPreview_Proxy(WeaponPreview instance)
-        {
-            __instance = instance;
-        }
 	}
 
-	public struct ItemSpecificationPanel_Proxy
+	public struct ItemSpecificationPanel_Proxy(ItemSpecificationPanel instance)
 	{
+        private readonly ItemSpecificationPanel __instance = instance;
+
 		private static TypedFieldInfo<ItemSpecificationPanel, Item> __item_0 = new("item_0");
 
 		public Item item_0 { get { return __item_0.Get(__instance); } set { __item_0.Set(__instance, value); } }
-
-        private ItemSpecificationPanel __instance;
-
-        public ItemSpecificationPanel_Proxy(ItemSpecificationPanel instance)
-        {
-            __instance = instance;
-        }
 	}
 
-	public struct InteractionButtonsContainer_Proxy
+	public struct InteractionButtonsContainer_Proxy(InteractionButtonsContainer instance)
 	{
+        private readonly InteractionButtonsContainer __instance = instance;
+
 		private static TypedFieldInfo<InteractionButtonsContainer, SimpleContextMenuButton> __buttonTemplate = new("_buttonTemplate");
 		private static TypedFieldInfo<InteractionButtonsContainer, RectTransform> __buttonsContainer = new("_buttonsContainer");
 
 		public SimpleContextMenuButton _buttonTemplate { get { return __buttonTemplate.Get(__instance); } set { __buttonTemplate.Set(__instance, value); } }
 		public RectTransform _buttonsContainer { get { return __buttonsContainer.Get(__instance); } set { __buttonsContainer.Set(__instance, value); } }
-
-        private InteractionButtonsContainer __instance;
-
-        public InteractionButtonsContainer_Proxy(InteractionButtonsContainer instance)
-        {
-            __instance = instance;
-        }
 	}
 
-    public struct ContextMenuButton_Proxy
+    public struct ContextMenuButton_Proxy(ContextMenuButton instance)
 	{
+        private readonly ContextMenuButton __instance = instance;
+
 		private static TypedFieldInfo<ContextMenuButton, TextMeshProUGUI> __text = new("_text");
 
 		public TextMeshProUGUI _text { get { return __text.Get(__instance); } set { __text.Set(__instance, value); } }
-
-        private ContextMenuButton __instance;
-
-        public ContextMenuButton_Proxy(ContextMenuButton instance)
-        {
-            __instance = instance;
-        }
 	}
 
 	public class Patch_PWA_method_23 : ModulePatch
