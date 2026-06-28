9bf95fd7cf51d5fba5ff780487d1f83b324e9e4f
7Bpencil <Edward.Ekb@yandex.ru>
2026-06-28 22:59:31 +0500

    bump version number

--- Client/Plugin.cs
+++ Client/Plugin.cs
@@ -75,7 +75,7 @@ namespace SevenBoldPencil.TransparentSights
 		MODES_COUNT,
 	}
 
-    [BepInPlugin("7Bpencil.TransparentSights", "7Bpencil.TransparentSights", "0.0.1")]
+    [BepInPlugin("7Bpencil.TransparentSights", "7Bpencil.TransparentSights", "0.1.0")]
     public class Plugin : BaseUnityPlugin
     {
         public static readonly int _Cull = Shader.PropertyToID("_Cull");
